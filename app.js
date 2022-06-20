ba8f022a64ec2865287f8c99380ba4b6088adef4
Rodrigo Calleja <Rodrigocalleja6@gmail.com>
2022-06-20 14:39:54 -0300

    Subiendo Mercado liebre completo

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, 'public'))
+);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.listen(3000, () => {
+    console.log('listening on http://localhost:3000');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
